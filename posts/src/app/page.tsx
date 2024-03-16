ad7ae26dafd755807bc2126e1905d11d53d56b58
KuzeKrott <christina.krot@yandex.ru>
2024-03-16 22:22:25 +0300

    first add project

---
+++ posts/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+import styles from "./page.module.scss";
+
+export default function Home() {
+  return (
+    <div>
+      <Typography variant="h2">
+        JOPAPISA
+      </Typography>
+    </div>
+  )
+}
